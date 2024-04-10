5b0827419ecd46cae65a7d76247cda4bfd648ab4
samuel <leumasmontreal@hotmail.com>
2024-04-10 14:35:52 -0400

    .

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.databaseinterface2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.databaseinterface2 to javafx.fxml;
+    exports org.example.databaseinterface2;
+}
